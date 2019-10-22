cc073368a14c75f0ee7728b0ed74edd353286324
siddarthm93 <40264657+siddarthm93@users.noreply.github.com>
2019-10-22 17:03:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,34 +1,84 @@
-const http = require('http');
-var fs = require('fs');
-var url = require('url');
+var express = require('express');
 
-const hostname = '127.0.0.1';
-const port = 3001;
+var app = express();
 
-const server = http.createServer((req, res) => {
-  
-  var pathname = url.parse(req.url).pathname;
+var session = require('express-session');
 
-  console.log("Request for " + pathname + " received.");
-  fs.readFile('index.html',function(err, data){
-  if(err)
-    {
-      console.log(err);
-      res.writeHead(404,{'Content-Type':'text/html'});
-  }
-  else
-  {
-  
-  res.statusCode = 200 ;
-  res.writeHeader(200,'Content-Type', 'text/html');
-  res.write(data.toString());
-  res.end();
-}
+// import body parser
+
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({
+
+    extended: true
+
+}));
+
+
+
+app.use(bodyParser.json());
+
+//render static content in express
+
+app.use('/common_lib', express.static('common_lib'));
+
+//render user login page
+
+app.get('/', function(req, res) {
+
+    res.sendFile(__dirname + '/ImageCaptcha.html');
+
+});
+
+app.use(session({
+
+    secret: '12345689'
+
+}));
+
+//validate user and and redirect
+
+app.post('/', function(req, res) {
+
+    var username = req.body.username;
+
+    var password = req.body.password;
+
+    var user_captcha = req.body.user_captcha; // from user entered Captcha Text
+
+    var captcha_Origing = req.body.captcha_Origing; // this is from hidden input html form
+
+    user_captcha
+
+    var org_username = 'test@gmail.com';
+
+    var org_password = '123456';
+
+    if (username === org_username && password === org_password && user_captcha === captcha_Origing) {
+
+        req.session.user = username;
+
+        return res.redirect('/user');
+
+    } else {
+
+        return res.redirect('/');
+
+    }
+
 });
 
 
+app.get('/user', function(req, res) {
+
+    res.end('<h1> Hey ' + req.session.user + ' You Entered Valid Captcha !</h1>');
+
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+
+
+app.listen(3000, function() {
+
+    console.log('server started');
+
 });
